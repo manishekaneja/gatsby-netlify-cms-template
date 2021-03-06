0ee46b282e47676431658023a45b389fd34fa426
Manish Aneja <manishekaneja@gmail.com>
2021-03-06 20:53:28 +0530

    Different approch to apply tailwind css

--- src/components/HTMLContent.js
+++ src/components/HTMLContent.js
@@ -3,5 +3,12 @@ import React from 'react';
 export const HTMLContent = ({ content, className = '' }) => (
   <>
     <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
+    <p className="text-2xl font-semibold mb-5 text-red-300 sm:text-3xl hidden"></p>
+    <p className="ml-10 list-decimal my-2 text-yellow-200 sm:my-5 hidden"></p>
+    <p className="mb-2 sm:text-lg hidden"></p>
+    <p className="text-base text-gray-100 mb-6 sm:text-lg hidden"></p>
+    <p className="text-green-200 my-6 hidden"></p>
+    <p className="text-blue-200 underline text-semibold italic hidden"></p>
+
   </>
 );
