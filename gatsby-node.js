47a0840bf9fab20f9fcb6ebded4a0aa1ec74a5cc
Manish Aneja <manishekaneja@gmail.com>
2021-06-26 19:55:41 +0530

    🔥 Remove extra values of project

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,48 +2,63 @@ const path = require('path');
 const {createFilePath} = require('gatsby-source-filesystem');
 const {fmImagesToRelative} = require('gatsby-remark-relative-images');
 
-exports.createPages = ({actions, graphql}) => {
+// exports.sourceNodes = ({actions}) => {
+//   actions.createTypes(`
+//   type markdownRemark implements Node @dontInfer{
+//     collection:String!
+//   }`);
+// };
+
+// exports.createResolvers = ({createResolvers, getNode}) => {
+//   const collection = (source) => getNode(source.parent).relativeDirectory;
+
+//   createResolvers({
+//     MD: {
+//       collection: {
+//         resolve: (source) => collection(source),
+//       },
+//     },
+//   });
+// };
+
+exports.createPages = ({actions, graphql, reporter}) => {
   const {createPage} = actions;
 
   return graphql(`
     query CreatePageQuery {
-      allMarkdownRemark(limit: 1000) {
+      allMarkdownRemark {
         edges {
           node {
-            id
-            internal {
-              content
-            }
             frontmatter {
-              description
-              name
               projectId
-              techStack
-              title
             }
           }
         }
       }
     }
-  `).then((result) => {
-    if (result.errors) {
-      result.errors.forEach((e) => console.error(e.toString()));
-      return Promise.reject(result.errors);
-    }
+  `)
+    .then((result) => {
+      if (result.errors) {
+        result.errors.forEach((e) => console.error(e.toString()));
+        return Promise.reject(result.errors);
+      }
+      const projectList = result.data.allMarkdownRemark.edges;
 
-    const projets = result.data.allMarkdownRemark.edges;
-
-    projets.forEach((singleProject) => {
-      const id = singleProject.node.frontmatter.projectId;
-      createPage({
-        path: id,
-        component: path.resolve(`src/screen-render/project-page.js`),
-        context: {
-          id,
-        },
+      projectList.forEach((singleProject) => {
+        const projectId = singleProject.node.frontmatter.projectId;
+        createPage({
+          path: projectId,
+          component: path.resolve(`src/screen-render/project-page.js`),
+          context: {
+            id: projectId,
+          },
+        });
       });
+    })
+    .catch((error) => {
+      reporter.panic('Error While creating Page', error);
+      return Promise.reject(error);
     });
-  });
 };
 
 exports.onCreateNode = ({node, actions, getNode}) => {
